6f7eeea6b7e98f69c4d29c3c0c30067b6ce4d2d7
Bogdan <bogdan.caliman112@gmail.com>
2021-01-02 01:08:51 +0200

    prettier

--- PW/math-kidz/components/quiz.js
+++ PW/math-kidz/components/quiz.js
@@ -1,24 +1,28 @@
-import Link from "next/link";
-import styles from "./quiz.module.scss";
+import Link from 'next/link';
+import styles from './quiz.module.scss';
 
 export default function Quiz() {
-  return (
-      <div className={styles.body}>
-      <div class ="container">
-            <div id="question-container" class="hide">
-                <div id="question">Question</div>
-                <div id="answer-buttons" class="btn-grid">
-                    <button class="btn">Answer 1</button>
-                    <button class="btn">Answer 2</button>
-                    <button class="btn">Answer 3</button>
-                    <button class="btn">Answer 4</button>
-                </div>
-            </div>
-            <div class="controls">
-                <button id="start-btn" class="start-btn btn">Start</button>
-                <button id="next-btn" class="next-btn btn hide">Next</button>
-            </div>
-      </div>
-      </div>
-  );
+	return (
+		<div className={styles.body}>
+			<div class="container">
+				<div id="question-container" class="hide">
+					<div id="question">Question</div>
+					<div id="answer-buttons" class="btn-grid">
+						<button class="btn">Answer 1</button>
+						<button class="btn">Answer 2</button>
+						<button class="btn">Answer 3</button>
+						<button class="btn">Answer 4</button>
+					</div>
+				</div>
+				<div class="controls">
+					<button id="start-btn" class="start-btn btn">
+						Start
+					</button>
+					<button id="next-btn" class="next-btn btn hide">
+						Next
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
--- PW/math-kidz/pages/cursuri/index.js
+++ PW/math-kidz/pages/cursuri/index.js
@@ -1,28 +1,30 @@
-import Layout from "../../components/layout";
-import Cursuri from "../../components/cursuri";
-import isAuthenticated from "../../utils/auth"
+import Layout from '../../components/layout';
+import Cursuri from '../../components/cursuri';
+import isAuthenticated from '../../utils/auth';
 export default function PaginaCursuri() {
-   let numeUtilizator = null;
+	let numeUtilizator = null;
 
-  if (typeof localStorage !== "undefined") {
-    numeUtilizator = JSON.parse(localStorage.getItem("user")).username;
-  }
-  return (
-    <Layout>
-      {
-        isAuthenticated()?<Cursuri>
-        <div
-          style={{
-            top: "30%",
-            position: "absolute",
-            width: "50%",
-            marginLeft: "auto",
-          }}
-        >
-          <h1>Alege un curs, {numeUtilizator}</h1>
-        </div>
-      </Cursuri>:<p>Logheaza-te ca sa vezi cursurile</p>
-      }
-    </Layout>
-  );
+	if (typeof localStorage !== 'undefined') {
+		numeUtilizator = JSON.parse(localStorage.getItem('user')).username;
+	}
+	return (
+		<Layout>
+			{isAuthenticated() ? (
+				<Cursuri>
+					<div
+						style={{
+							top: '30%',
+							position: 'absolute',
+							width: '50%',
+							marginLeft: 'auto',
+						}}
+					>
+						<h1>Alege un curs, {numeUtilizator}</h1>
+					</div>
+				</Cursuri>
+			) : (
+				<p>Logheaza-te ca sa vezi cursurile</p>
+			)}
+		</Layout>
+	);
 }
